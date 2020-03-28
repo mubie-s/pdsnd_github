cc13f69bde3e0ced4b37099a6c9be452ff1505e6
Mubaraq <sanusimubaraq44@gmail.com>
2020-03-28 13:34:06 +0100

    Remove Hints in TO DO Comments

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington)
     while True:
         city = input("Enter city name: ")
         if city in CITY_DATA:
